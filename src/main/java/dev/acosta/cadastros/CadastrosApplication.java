99c210a2a82153ed8f449cdcb1c28c500fd893fe
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-24 07:28:33 -0300

    configures automatic scan of models and creation of tables in database

--- src/main/java/dev/acosta/cadastros/CadastrosApplication.java
+++ src/main/java/dev/acosta/cadastros/CadastrosApplication.java
@@ -2,8 +2,10 @@ package dev.acosta.cadastros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages="dev.acosta.model") //permite a criação automática de tabelas
 public class CadastrosApplication {
 
 	public static void main(String[] args) {
