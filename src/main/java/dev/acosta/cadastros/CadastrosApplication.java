ade17d2e5de5f2e18eba1211cf7d2a9af2087141
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-25 06:01:47 -0300

    forces the mapping of all directories in dev.acosta so the app can find the controller for cadastro-de-pessoa. Adds a link to the register page in index

--- src/main/java/dev/acosta/cadastros/CadastrosApplication.java
+++ src/main/java/dev/acosta/cadastros/CadastrosApplication.java
@@ -3,9 +3,11 @@ package dev.acosta.cadastros;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages="dev.acosta.model") //permite a criação automática de tabelas
+@ComponentScan(basePackages = {"dev.acosta.*"}) //força o mapeamento das pastas indicadas
 public class CadastrosApplication {
 
 	public static void main(String[] args) {
