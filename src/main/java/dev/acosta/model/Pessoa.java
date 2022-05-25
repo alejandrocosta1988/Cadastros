3000ad9f130c373c45810c2bb645cf3ec1f59680
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-25 06:09:32 -0300

    creates a repository for Pessoa. Corrects the type of id to Long

--- src/main/java/dev/acosta/model/Pessoa.java
+++ src/main/java/dev/acosta/model/Pessoa.java
@@ -14,7 +14,7 @@ public class Pessoa implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	private Long id;
 	
 	private String nome;
 	private String sobrenome;
---
+++ src/main/java/dev/acosta/repositories/PessoaRepository.java
@@ -0,0 +1,7 @@
+package dev.acosta.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PessoaRepository extends CrudRepository<T, ID> {
+
+}
