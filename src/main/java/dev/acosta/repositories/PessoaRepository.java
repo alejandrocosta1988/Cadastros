4ebbf8a25ea7ae57f5f1f685d62046da3c9d4388
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-25 06:10:16 -0300

    repository to Pessoa and type of id Long

--- src/main/java/dev/acosta/repositories/PessoaRepository.java
+++ src/main/java/dev/acosta/repositories/PessoaRepository.java
@@ -1,7 +1,13 @@
 package dev.acosta.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface PessoaRepository extends CrudRepository<T, ID> {
+import dev.acosta.model.Pessoa;
+
+@Repository
+@Transactional
+public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
 
 }
