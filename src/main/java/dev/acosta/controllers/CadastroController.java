038c4a0f1391f2c4a732c77b508d4695b5232520
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-30 16:07:00 -0300

    adds possibility to exclude registers from the database

--- src/main/java/dev/acosta/controllers/CadastroController.java
+++ src/main/java/dev/acosta/controllers/CadastroController.java
@@ -2,9 +2,9 @@ package dev.acosta.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -50,6 +50,14 @@ public class CadastroController {
 		modelAndView.addObject("pessoa", pessoa);
 		
 		return modelAndView;
+		
+	}
+	
+	@GetMapping("/excluir-pessoa/{id}")
+	public ModelAndView excluirPessoa(@PathVariable("id") Long id) {
+		
+		pessoaRepository.deleteById(id);
+		return pessoas();
 		
 	}
 	
