9be9a4ba96c6f08abe4ef3de8cb0c41c6368592f
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-30 16:40:01 -0300

    adds method findPessoaByName in PessoaRepository

--- src/main/java/dev/acosta/repositories/PessoaRepository.java
+++ src/main/java/dev/acosta/repositories/PessoaRepository.java
@@ -1,5 +1,8 @@
 package dev.acosta.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +13,7 @@ import dev.acosta.model.Pessoa;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
 
+	@Query("SELECT pessoa FROM Pessoa pessoa WHERE pessoa.nome LIKE	%1%")
+	List<Pessoa> findPessoaByName(String nome);
+	
 }
