60cdc1ed0a26267d4a1cb9d9f15ac714442595d5
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-31 12:12:58 -0300

    corrects the query using named param

--- src/main/java/dev/acosta/repositories/PessoaRepository.java
+++ src/main/java/dev/acosta/repositories/PessoaRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +14,7 @@ import dev.acosta.model.Pessoa;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
 
-	@Query("SELECT pessoa FROM Pessoa pessoa WHERE pessoa.nome LIKE	%1%")
-	List<Pessoa> findPessoaByName(String nome);
+	@Query("SELECT p FROM Pessoa p WHERE p.nome LIKE CONCAT('%', :nome, '%')")
+	List<Pessoa> findPessoaByNome(@Param("nome") String nome);
 	
 }
