675e311af9db2ba871a6d24e8ee65fd8daf51512
Alejandro Costa <alejandroesfc@gmail.com>
2022-06-01 06:57:26 -0300

    adds method to get the telephone numbers of a person

--- src/main/java/dev/acosta/repositories/TelefoneRepository.java
+++ src/main/java/dev/acosta/repositories/TelefoneRepository.java
@@ -1,9 +1,16 @@
 package dev.acosta.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import dev.acosta.model.Telefone;
 
 public interface TelefoneRepository extends CrudRepository<Telefone, Long> {
 
+	@Query("SELECT telefone FROM Telefone telefone WHERE telefone.pessoa.id = :pessoaId")
+	List<Telefone> getTelefones(@Param("pessoaId") Long pessoaId);
+	
 }
