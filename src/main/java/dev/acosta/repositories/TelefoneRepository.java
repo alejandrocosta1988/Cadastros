a8b15217a839b0daf352be0f56fa454c739dcb16
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-31 21:22:50 -0300

    creates repository for Telefone

---
+++ src/main/java/dev/acosta/repositories/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package dev.acosta.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dev.acosta.model.Telefone;
+
+public interface TelefoneRepository extends CrudRepository<Telefone, Long> {
+
+}
