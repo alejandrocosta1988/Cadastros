d97f4817148f68fb5bb58ecce6b88eb9722ccbb4
Alejandro Costa <alejandroesfc@gmail.com>
2022-06-06 17:22:18 -0300

    implements authetication by reading user and encrypted password from database

--- src/main/java/dev/acosta/security/WebConfigSecurity.java
+++ src/main/java/dev/acosta/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package dev.acosta.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,13 +8,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsServiceImplementation userDetailsServiceImplementation;
+	
 	@Override //configura as solicitações de acesso por http
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
@@ -28,10 +32,8 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override //cria autenticação do usuário com banco de dados ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
-			.withUser("acosta")
-			.password("123")
-			.roles("ADMIN");
+		auth.userDetailsService(userDetailsServiceImplementation)
+		    .passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	@Override
